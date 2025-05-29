0019dd10ec146333b1be1e82f3a868d386c82797
VieiraGuilherme-dev <gv524003@gmail.com>
2025-05-28 23:19:46 -0300

    tabela usuario criada

---
+++ src/main/java/com/bancodedados/gestaoespaco/model/Usuario.java
@@ -0,0 +1,66 @@
+package com.bancodedados.gestaoespaco.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String tipo;
+
+    public Usuario() {
+    }
+
+    // Construtor com parâmetros
+    public Usuario(String nome, String email, String tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.tipo = tipo;
+    }
+
+    // Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
+}
