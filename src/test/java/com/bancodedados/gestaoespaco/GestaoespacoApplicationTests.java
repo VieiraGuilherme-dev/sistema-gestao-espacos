0a0ae6366f8ad3c91cfc43d51108612ae988bcf0
VieiraGuilherme-dev <gv524003@gmail.com>
2025-05-28 23:12:37 -0300

    project created

---
+++ src/test/java/com/bancodedados/gestaoespaco/GestaoespacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancodedados.gestaoespaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoespacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
